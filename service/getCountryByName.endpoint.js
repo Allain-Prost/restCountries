654a43e66d98d1fbcd410b35a8bd2784f3fefebd
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-03-25 13:58:19 -0300

    Ajustando o método  getCountryByName

--- service/getCountryByName.endpoint.js
+++ service/getCountryByName.endpoint.js
@@ -1,9 +1,9 @@
 const { spec } = require("pactum");
 
-async function getCountryByName(name){
+async function getCountryByName(name,status){
     return await spec()
         .get(`https://restcountries.com/v3.1/name/${name}`)
-        .expectStatus(200)
+        .expectStatus(status)
 }
 
 module.exports = {
